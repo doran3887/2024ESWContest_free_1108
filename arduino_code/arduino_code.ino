d7104afdbdf5419bfdc944ddeda523ce0c7b3dc0
doran3887 <96973983+doran3887@users.noreply.github.com>
2024-09-05 01:34:49 +0900

    Update arduino_code.ino

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -2,14 +2,8 @@
 #include <MightyZap.h>
 
 #define ID_NUM 1
-#define POSITION1 0
-#define POSITION2 2730
 #define SPEED 1023
-#define MOVEMENT_DELAY 2000 // 2초 동안 움직임이 완료되도록 딜레이 설정
 IRROBOT_EZController Easy(&Serial1);
-
-int positions[3] = {0, 0, 0}; // 이전 위치를 저장할 배열
-int currentIndex = 0; // 현재 인덱스를 추적할 변수
 
 void setup() {
   Serial.begin(9600);
