139b162fc06e51318e1153b3ea856c99edbfa756
doran3887 <96973983+doran3887@users.noreply.github.com>
2024-09-05 01:29:43 +0900

    Delete YOLOv10.py

--- YOLOv10.py
+++
@@ -1,5 +0,0 @@
-from ultralytics  import YOLO
-
-model  = YOLO('YOLOv10_model/best.pt')
-source = 'test1.mp4'
-model.predict(source = source, save = True)                                                                                                                                 
